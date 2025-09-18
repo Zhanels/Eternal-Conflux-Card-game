5dd724d8ad6aef119a57a5acc4754a5219d3383c
Zhanels <99288612+Zhanels@users.noreply.github.com>
2025-09-18 10:58:04 +0200

    18/09/2025 deck update
    added more cards so the deck shows less then 5 cards in deck gets drawn so it instantly hides deck because no more cards

--- Scripts/Deck.gd
+++ Scripts/Deck.gd
@@ -7,7 +7,7 @@ const CARD_DRAW_SPEED = 0.2
 const STARTING_HAND_SIZE = 5
 
 # Array met alle kaarten in het deck (duplicaten mogelijk)
-var player_deck = ["Knight", "Archer", "Demon","Knight","Tornado"]
+var player_deck =  ["Knight", "Archer", "Demon", "Knight", "Tornado", "Archer", "Knight", "Demon", "Archer", "Knight"]
 # Referentie naar de kaart database voor stats
 var card_database_reference
 # Boolean om te voorkomen dat meerdere kaarten per beurt getrokken worden
